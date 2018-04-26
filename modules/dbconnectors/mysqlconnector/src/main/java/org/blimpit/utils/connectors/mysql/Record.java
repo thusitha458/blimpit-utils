054d4bf733f0eb927d1f10098d80e45d7a5a8211
isururanawaka <irjanith@gmail.com>
2018-04-26 11:14:47 +0530

    Adding Record class

--- modules/dbconnectors/mysqlconnector/src/main/java/org/blimpit/utils/connectors/mysql/Record.java
+++ modules/dbconnectors/mysqlconnector/src/main/java/org/blimpit/utils/connectors/mysql/Record.java
@@ -4,9 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *A  class represents a record of a table in DB
+ * A  class represents a record of a table in DB
  */
 public class Record {
+
 
     private int index;
 
@@ -17,18 +18,31 @@ public class Record {
         this.recordAttributes = new HashMap<String, String>();
     }
 
+    /**
+     * poistion of the table
+     *
+     * @return index
+     */
     public int getIndex() {
         return index;
     }
 
-    public void setIndex(int index) {
-        this.index = index;
-    }
 
+    /**
+     * Returns map of fieldnames and values
+     *
+     * @return recordAttributes
+     */
     public Map<String, String> getRecordAttributes() {
         return recordAttributes;
     }
 
+    /**
+     * Add fieldname and value to the record
+     *
+     * @param fieldName
+     * @param value
+     */
     public void addRecordAttribute(String fieldName, String value) {
         this.recordAttributes.put(fieldName, value);
     }
